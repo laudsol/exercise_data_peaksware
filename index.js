1fcb38c1ab98dbd016fd5953bca6485dd93d3586
sol laudon <blaudon1@gmail.com>
2018-04-15 15:26:36 -0600

    refactor for simple calc complete

--- index.js
+++ index.js
@@ -1,97 +1,40 @@
-const oneMinute = 60 * 1000
-let powerArray = []
-let beginTime = 1000
+let powerData = workoutData.samples
+let oneMinute = 60
+let timeInput = 20 * oneMinute
 
-let oneMinAvgs = []
-
-workoutData.samples.forEach(second => {
-    if((second.millisecondOffset - beginTime + 1000) <= oneMinute){
-        powerArray.push(second.values.power)
-    } else {
-        let powerObj = {
-            begin: beginTime/1000,
-            end: (second.millisecondOffset - 1000)/1000,
-            avgPower: powerArray.reduce((acc, curr) => acc + curr) / powerArray.length
-        }
-
-        oneMinAvgs.push(powerObj)
-        powerArray = [second.values.power]
-        beginTime = second.millisecondOffset
-    }
-})
-
-// oneMinAvgs.forEach((minute, i) => console.log(i, minute))
-
-let twentyMinuteAverages = []
-
-for(let i = 0; i <= oneMinAvgs.length - 20; i++){
-    let tempAvgArr = []
+const itteratePowerData = (powerData, timeInput) => {
+    let timeSegmentedPowerAverage = []
+    let maxAvgPowerOverTimeSegment = {avgPower: 0}
     
-    for(let j = 0; j < 20; j++){
-        if(j === 0){
-            tempAvgArr.push(oneMinAvgs[i + j])
-        } else if ((tempAvgArr[0].begin - oneMinAvgs[i + j].begin) <= (20 * 60)){
-            tempAvgArr.push(oneMinAvgs[i + j])
+    for(let i = 0; i < powerData.length; i++){
+        let avgPowerObj = {
+            start: powerData[i].millisecondOffset/1000,
+            end: '',
+            avgPower: '',
+        }
+
+        let avgPowerCalc = []
+
+        for(let j = i; j < powerData.length - i; j++){
+            if(avgPowerObj.start + timeInput > powerData[j].millisecondOffset/1000){
+
+                avgPowerCalc.push(powerData[j].values.power)
+                avgPowerObj.end = powerData[j].millisecondOffset/1000
+            }
+        }
+
+        if(avgPowerCalc.length > 0 && avgPowerObj.start + timeInput -1 === avgPowerObj.end){
+            avgPowerObj.avgPower = avgPowerCalc.reduce((acc, curr) => {return acc + curr}) / avgPowerCalc.length
+            timeSegmentedPowerAverage.push(avgPowerObj)
+        }
+
+        if(avgPowerObj.avgPower > maxAvgPowerOverTimeSegment.avgPower){
+            maxAvgPowerOverTimeSegment = avgPowerObj
         }
     }
-    let powerCounter = 0
-    let counter = 0 
-
-    tempAvgArr.forEach(minute => {
-        powerCounter += minute.avgPower
-        counter += 1
-    })
-
-    let tempObj = {
-        start: oneMinAvgs[i].begin,
-        end: oneMinAvgs[i+19].end,
-        avgPower: powerCounter / counter
-    }
-
-    twentyMinuteAverages.push(tempObj)
+    return [timeSegmentedPowerAverage, maxAvgPowerOverTimeSegment]
 }
 
+let timeSegmentedPowerdata = itteratePowerData(powerData, timeInput)
 
-let highestTwentyMinuteEstimate = twentyMinuteAverages[0]
-
-twentyMinuteAverages.forEach((timeSpan,i) => {
-    if(timeSpan.avgPower > highestTwentyMinuteEstimate.avgPower){
-        highestTwentyMinuteEstimate = timeSpan 
-    }
-})
-
-let oneSecondIntervals = []
-let powerCounter
-let start
-let end
-let max = {avgPower: 0}
-
-workoutData.samples.forEach((sec,i) => {
-    if(sec.millisecondOffset >= highestTwentyMinuteEstimate.start - (oneMinute * 20) && sec.millisecondOffset <= highestTwentyMinuteEstimate.end + (oneMinute * 20)){
-        start = sec.millisecondOffset
-        end = start + (oneMinute * 20)
-        powerCounter = []
-        let shortenedArray = workoutData.samples.slice(i)
-
-            shortenedArray.forEach(shortSecond => {
-                if(shortSecond.millisecondOffset <= end){
-                    powerCounter.push(shortSecond.values.power)
-                } else {
-                    let powerObj = {
-                        begin: start,
-                        end: end,
-                        avgPower: powerCounter.reduce((acc,curr) => acc + curr) / powerCounter.length
-                    }
-
-                    oneSecondIntervals.push(powerObj)
-                    if(max.avgPower < powerObj.avgPower){
-                        max = powerObj
-                    }
-                }
-                // console.log('oneSecondIntervals', oneSecondIntervals)
-            })
-    }
-})
-
-// console.log('oneSecondIntervals', oneSecondIntervals)
-
+console.log(timeSegmentedPowerdata)
---
+++ old.js
@@ -0,0 +1,92 @@
+const oneMinute = 60 * 1000
+let powerArray = []
+let beginTime = 1000
+
+let oneMinAvgs = []
+
+workoutData.samples.forEach(second => {
+    if((second.millisecondOffset - beginTime + 1000) <= oneMinute){
+        powerArray.push(second.values.power)
+    } else {
+        let powerObj = {
+            begin: beginTime/1000,
+            end: (second.millisecondOffset - 1000)/1000,
+            avgPower: powerArray.reduce((acc, curr) => acc + curr) / powerArray.length
+        }
+
+        oneMinAvgs.push(powerObj)
+        powerArray = [second.values.power]
+        beginTime = second.millisecondOffset
+    }
+})
+
+let twentyMinuteAverages = []
+
+for(let i = 0; i <= oneMinAvgs.length - 20; i++){
+    let tempAvgArr = []
+    
+    for(let j = 0; j < 20; j++){
+        if(j === 0){
+            tempAvgArr.push(oneMinAvgs[i + j])
+        } else if ((tempAvgArr[0].begin - oneMinAvgs[i + j].begin) <= (20 * 60)){
+            tempAvgArr.push(oneMinAvgs[i + j])
+        }
+    }
+    let powerCounter = 0
+    let counter = 0 
+
+    tempAvgArr.forEach(minute => {
+        powerCounter += minute.avgPower
+        counter += 1
+    })
+
+    let tempObj = {
+        start: oneMinAvgs[i].begin,
+        end: oneMinAvgs[i+19].end,
+        avgPower: powerCounter / counter
+    }
+
+    twentyMinuteAverages.push(tempObj)
+}
+
+
+let highestTwentyMinuteEstimate = twentyMinuteAverages[0]
+
+twentyMinuteAverages.forEach((timeSpan,i) => {
+    if(timeSpan.avgPower > highestTwentyMinuteEstimate.avgPower){
+        highestTwentyMinuteEstimate = timeSpan 
+    }
+})
+
+let oneSecondIntervals = []
+let powerCounter
+let start
+let end
+let max = {avgPower: 0}
+
+workoutData.samples.forEach((sec,i) => {
+    if(sec.millisecondOffset >= highestTwentyMinuteEstimate.start - (oneMinute * 20) && sec.millisecondOffset <= highestTwentyMinuteEstimate.end + (oneMinute * 20)){
+        start = sec.millisecondOffset
+        end = start + (oneMinute * 20)
+        powerCounter = []
+        let shortenedArray = workoutData.samples.slice(i)
+
+            shortenedArray.forEach(shortSecond => {
+                if(shortSecond.millisecondOffset <= end){
+                    powerCounter.push(shortSecond.values.power)
+                } else {
+                    let powerObj = {
+                        begin: start,
+                        end: end,
+                        avgPower: powerCounter.reduce((acc,curr) => acc + curr) / powerCounter.length
+                    }
+
+                    oneSecondIntervals.push(powerObj)
+                    if(max.avgPower < powerObj.avgPower){
+                        max = powerObj
+                    }
+                }
+            })
+    }
+})
+
